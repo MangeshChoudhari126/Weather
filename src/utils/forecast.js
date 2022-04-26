2994b3756abf160c2a23bf5b3d4e2651ba665bcc
MangeshChoudhari <mangeshchou96@gmail.com>
2022-04-26 16:50:58 +0530

    remove chalk from forecast file

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,4 @@
 const request = require('request')
-const chalk = require('chalk')
 const forecast = (latitude,longitude,callback)=>
 { 
     const url = "http://api.weatherstack.com/current?access_key=b982307a4fb5a6724b9a5611bb4d2e77&query="+latitude+","+longitude;
@@ -19,14 +18,6 @@ const forecast = (latitude,longitude,callback)=>
             callback(undefined, "The city "+body.location.name+" has a temperature "+
             body.current.temperature+" degree celcius."
             )
-        
-            // name = body.location.name;
-            // temperature = body.current.temperature;
-            // console.log(name);
-            //  console.log(body.location.country);
-            //  console.log(chalk.green("The city "+name+" has a temperature "+temperature+" degree celcius"));
-            //  console.log(chalk.cyan("Address: "+
-            //  name +","+body.location.region+","+body.location.country));
         }
     })
 }
